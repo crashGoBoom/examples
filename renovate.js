f9da74f43da0735c133d6185a43a48a963931848
Christopher Mundus <17323411+crashGoBoom@users.noreply.github.com>
2021-01-25 12:51:46 -0500

    Create renovate.js

---
+++ renovate.js
@@ -0,0 +1,38 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+module.exports = {
+  extends: ["config:base", ":semanticCommitsDisabled"],
+  renovateFork: true,
+  prConcurrentLimit: 30,
+  prHourlyLimit: 30,
+  prCreation: "immediate",
+  pinDigests: true,
+  onboarding: false,
+  logLevel: "debug",
+  requireConfig: false,
+  platform: "github",
+  gitAuthor: "KindlyMachine <support@kindlyops.com>",
+  dependencyDashboard: true,
+  reviewersFromCodeOwners: true,
+  enabledManagers: ["github-actions", "npm", "gomod"],
+  "github-actions": {
+    fileMatch: ["^\\.github/workflows/[^/]+\\.ya?ml$"],
+    pinDigests: true,
+    labels: ["dependencies", "github"],
+  },
+  gomod: {
+      labels: ["dependencies", "go"]
+  },
+  postUpdateOptions: ["gomodTidy"],
+  npm: {
+    labels: ["dependencies", "javascript"],
+  },
+  packageRules: [
+    {
+      packagePatterns: [ "constructs" ],
+      groupName: "aws-cdk monorepo",
+      schedule: ["on the first day of the month"],
+    },
+  ],
+};
